c6a41dcdc8e9c00cc90e3135c97fff460e8487c5
aneesh <aneeshchauhan0609@gmail.com>
2024-10-28 03:09:22 +0530

    personal header customization

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,70 +1,92 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <Navbar className="border-b-2 py-2 px-4">
-      <div className="flex items-center justify-between w-full">
-        {/* Logo and Title */}
-        <Link to="/" className="flex items-center space-x-2">
-          <span className="text-sm sm:text-xl ml-5 mr-5 font-semibold dark:text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white px-2 py-1">
-            Moltres's BLOG
-          </span>
-        </Link>
-        
-        {/* Search Bar for Desktop */}
-        <div className="hidden lg:flex flex-grow mx-4">
-          <TextInput 
-            type="text"
-            placeholder="Search..."
-            className="flex-grow"
-          />
-        </div>
-
-        {/* Search Button for Mobile */}
-        <Button className="lg:hidden w-11 h-8 rounded-full" color="gray">
-          <AiOutlineSearch className="w-4 h-4" />
+   
+    <Navbar className= 'border-b-2 md:inline-block w-full'>
+       <div className=' mx-auto flex  items-center flex-wrap justify-between container '>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Moltres's
+        </span>
+        Blog
+      </Link>
+      <form className='w-96 mt-2'>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          className='hidden lg:inline'
+        />
+      </form>
+   
+     
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex items-stretch '>
+        <Button
+          className='w-12 h-10 mt-2 hidden sm:inline'
+          color='gray'
+          pill
+        >
+         <FaMoon />
         </Button>
-
-        {/* Toggle Button for Mobile Menu */}
-        <Navbar.Toggle className="lg:hidden" />
-
-       
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded className='ml-8 mr-4' />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item >Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item >Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
       </div>
-  
-      {/* Navbar Links for Mobile */}
-      <Navbar.Collapse className="lg:inline">
-        <Navbar.Link active={path === "/home"} as={'div'}>
-          <Link to="/" className="block py-2 text-lg mr-10 ml-10">
-            Home
-          </Link>
+    
+      <Navbar.Collapse className='lg:inline sm:flex-wrap lg:ml-96 lg:text-lg lg:text-bold'>
+        <Navbar.Link active={path === '/'} as={'div'} className='lg:mr-24'>
+          <Link to='/'>Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={'div'}>
-          <Link to="/about" className="block py-2 text-lg mr-10">
-            About
-          </Link>
+        <Navbar.Link active={path === '/about'} as={'div'} className='lg:mr-24'>
+          <Link to='/about'>About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={'div'}>
-          <Link to="/projects" className="block py-2 text-lg mr-10">
-            Projects
-          </Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
-      
-
-       {/* Navbar Links and Theme Toggle */}
-       <div className="hidden lg:flex items-center  justify-end space-x-2">
-          <Button className="w-12 h-10 rounded-full pt-1" color="gray" pill>
-            <FaMoon />
-          </Button>
-          <Link to="/sign-in">
-            <Button   className='w-20 ml-5 dark:text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500' >Sign In</Button>
-          </Link>
-        </div>
+      </div>
+    
     </Navbar>
   );
 }
-
