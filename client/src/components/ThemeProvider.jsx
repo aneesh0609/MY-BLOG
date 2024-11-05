9d5f980f427a24cbde0ec0109fd8690298a21174
aneesh <aneeshchauhan0609@gmail.com>
2024-11-05 15:57:45 +0530

    dashboard private

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ export default function ThemeProvider({children}) {
   const {theme} = useSelector(state => state.theme)
   return (
      <div className={theme}>
-      <div className="bg-white text-gray-600 dark:text-gray-600 dark:bg-[rgb(27,26,26)] min-h-screen">
+      <div className="bg-white text-gray-600 dark:text-white dark:bg-[rgb(27,26,26)] min-h-screen">
       {children}
       </div>     
      </div>
