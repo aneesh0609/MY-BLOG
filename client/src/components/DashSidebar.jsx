ae0aa76409fb18e48cdbc7bdf0ae4481de41e04b
aneesh <aneeshchauhan0609@gmail.com>
2024-12-29 01:08:44 +0530

    add posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,15 +1,17 @@
 import { Sidebar } from 'flowbite-react'
-import { HiArrowSmRight, HiUser } from 'react-icons/hi'
+import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi'
 import  { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import React from 'react'
 import { signoutSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
   const location = useLocation()
   const [tab,setTab] = useState('');
   const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search)
     const tabFromUrl = urlParams.get('tab')
@@ -37,14 +39,27 @@ export default function DashSidebar() {
   return (
    <Sidebar className=' w-full md:w-56'>
     <Sidebar.Items>
-      <Sidebar.ItemGroup className='bg-gray-100 rounded-md  dark:text-white dark:bg-black'>
+      <Sidebar.ItemGroup className='bg-gray-100 rounded-md  dark:text-white dark:bg-black flex flex-col'>
         <Link to='/dashboard?tab=profile'>
-        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor ='dark' className='border-2  dark:text-white dark:bg-black'
+        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor ='dark' className='border-2  dark:text-white dark:bg-black'
         as='div'
         >
           Profile
         </Sidebar.Item>
         </Link>
+        {currentUser.isAdmin && (
+        <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                className='border-2  dark:text-white dark:bg-black'
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+ )}
+
         <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} className='cursor-pointer border-2' >
           Sign Out
         </Sidebar.Item>
