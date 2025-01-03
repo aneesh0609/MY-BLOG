cb4454d19c639633b4e58402d5d870b49d2125e5
aneesh <aneeshchauhan0609@gmail.com>
2025-01-03 16:39:37 +0530

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react'
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import  { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import React from 'react'
@@ -40,6 +40,17 @@ export default function DashSidebar() {
    <Sidebar className=' w-full md:w-56'>
     <Sidebar.Items>
       <Sidebar.ItemGroup className='bg-gray-100 rounded-md  dark:text-white dark:bg-black flex flex-col'>
+      {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
         <Link to='/dashboard?tab=profile'>
         <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor ='dark' className='border-2  dark:text-white dark:bg-black'
         as='div'
