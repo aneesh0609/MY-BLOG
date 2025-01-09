0b69894b40163622ea8522d2c471c4f944bf2366
aneesh <aneeshchauhan0609@gmail.com>
2025-01-09 22:53:37 +0530

    again push tailwind file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
-    require('tailwind-scrollbar'),
+    flowbitePlugin(),
+    tailwindScrollbar,
   ],
-};
+}
