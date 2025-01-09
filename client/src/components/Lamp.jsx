222893e8f182105eec0c0018137c0a603e8fa222
aneesh <aneeshchauhan0609@gmail.com>
2025-01-09 21:48:06 +0530

    do some minor changes in config and lamp

--- client/src/components/Lamp.jsx
+++ client/src/components/Lamp.jsx
@@ -15,15 +15,14 @@ import { motion } from "framer-motion";
  * Desktop navbar is better positioned at the bottom
  * Mobile navbar is better positioned at bottom right.
  **/
-import { IconBadge4k, IconBrandGoogleFilled, IconBrandInstagramFilled, IconBrandXFilled, IconLayoutNavbarCollapse, IconPawFilled } from "@tabler/icons-react";
+import {  IconBrandGoogleFilled,  IconBrandXFilled, IconLayoutNavbarCollapse, IconPawFilled } from "@tabler/icons-react";
 import { AnimatePresence,  useMotionValue, useSpring, useTransform } from "framer-motion";
 import {
   IconBrandGithub,
-  IconBrandX,
-  IconHome,
+
 } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
-import { Button } from "flowbite-react";
+
 
 
 export function FloatingDockDemo() {
@@ -533,160 +532,3 @@ export const useMouseEnter = () => {
 
 
 
-export const Card = React.memo(({
-  card,
-  index,
-  hovered,
-  setHovered
-}) => (
-  <div
-    onMouseEnter={() => setHovered(index)}
-    onMouseLeave={() => setHovered(null)}
-    className={cn(
-      "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
-      hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
-    )}>
-    <Image
-      src={card.src}
-      alt={card.title}
-      fill
-      className="object-cover absolute inset-0" />
-    <div
-      className={cn(
-        "absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300",
-        hovered === index ? "opacity-100" : "opacity-0"
-      )}>
-      <div
-        className="text-xl md:text-2xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
-        {card.title}
-      </div>
-    </div>
-  </div>
-));
-
-Card.displayName = "Card";
-
-export function FocusCards({
-  cards
-}) {
-  const [hovered, setHovered] = useState(null);
-
-  return (
-    (<div
-      className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full">
-      {cards.map((card, index) => (
-        <Card
-          key={card.title}
-          card={card}
-          index={index}
-          hovered={hovered}
-          setHovered={setHovered} />
-      ))}
-    </div>)
-  );
-}
-
-
-
-
-
-
-
-export function LampDemo() {
-  return (
-    (<LampContainer>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 0 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-Your trusted source for <br /> informative articles and educational resources 
-      </motion.h1>
-    </LampContainer>)
-  );
-}
-
-export const LampContainer = ({
-  children,
-  className
-}) => {
-  return (
-    (<div
-      className={cn(
-        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
-        className
-      )}>
-      <div
-        className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
-        <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "30rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          style={{
-            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-          }}
-          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]">
-          <div
-            className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div
-            className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "30rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          style={{
-            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-          }}
-          className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]">
-          <div
-            className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div
-            className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-        </motion.div>
-        <div
-          className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
-        <div
-          className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div
-          className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
-        <motion.div
-          initial={{ width: "8rem" }}
-          whileInView={{ width: "16rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"></motion.div>
-        <motion.div
-          initial={{ width: "15rem" }}
-          whileInView={{ width: "30rem" }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 "></motion.div>
-
-        <div
-          className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
-      </div>
-      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
-        {children}
-      </div>
-    </div>)
-  );
-};
